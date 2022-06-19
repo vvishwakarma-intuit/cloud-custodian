7bee8fb9bcff37e49b49e5f07f1a18ade73713e0
vvishwakarma <vivekanand_vishwakarma@intuit.com>
2022-06-19 20:50:26 +0530

    Revert "aws-create filter for cross-az-nat-gateway-route from table"
    This reverts commit 3e67c0b10ffe7eb5e04f9e908f660345171e7ae5.

--- c7n/resources/vpc.py
+++ c7n/resources/vpc.py
@@ -2688,7 +2688,11 @@ class CrossAZRouteTable(Filter):
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
               - name: cross-az-nat-gateway-traffic
+=======
+              - name: cross-az-nat-gw-traffic
+>>>>>>> parent of 3e67c0b10 (aws-create filter for cross-az-nat-gateway-route from table)
 =======
               - name: cross-az-nat-gw-traffic
 >>>>>>> parent of 3e67c0b10 (aws-create filter for cross-az-nat-gateway-route from table)
